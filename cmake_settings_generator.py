eb44a626fbdf5795cc4c2e6e251a259c52ac0b98
Justice Adams <adamsjal@uni.edu>
2021-01-19 20:39:40 -0800

    outline future work for parsing mechanism

--- cmake_settings_generator.py
+++ cmake_settings_generator.py
@@ -39,3 +39,9 @@ class BuildConfig:
     buildCommandArgs: str
     ctestCommandArgs: str
     type: CmakeVarType
+
+
+#ToDo: 1.) Create an interface to parse user input to the data classes with error checking (multiple buildconfigs)
+#ToDo: 2.) Parse data classes to JSON
+#ToDo: 3.) Write json to CMAKEList folder
+#ToDo: 3.) Notify the user that they can acheive the same resulst with a constructed Cmake command (in terms of build systems)
