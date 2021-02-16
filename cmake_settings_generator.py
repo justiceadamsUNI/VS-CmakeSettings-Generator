5d8b68aaf69bf27fb8880474ed48dc78b8b71b9e
Justice Adams <adamsjal@uni.edu>
2021-02-15 16:17:08 -0800

    start working on user input parsing before writing JSON file

--- cmake_settings_generator.py
+++ cmake_settings_generator.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
 from enum import Enum, auto
+from typing import List, TypeVar, Generic
+import json
 
 
 class NameableEnum(Enum):
@@ -28,20 +30,77 @@ class CmakeVariable:
 
 @dataclass(frozen=True)
 class BuildConfig:
-    """Data container for representing a Cmake Variable within the JSON file."""
+    """Data container for representing a CMAKE Build Configuration within the JSON file."""
     name: str
     generator: str
     configurationType: str
     inheritEnvironments: str  #ToDo: Convert to an enum after finding microsoft API doc
-    buildRoot: str
     installRoot: str
     cmakeCommandArgs: str
     buildCommandArgs: str
     ctestCommandArgs: str
-    type: CmakeVarType
+    variables: List[CmakeVariable]
+    buildRoot: str
+    installRoot: str
 
 
-#ToDo: 1.) Create an interface to parse user input to the data classes with error checking (multiple buildconfigs)
-#ToDo: 2.) Parse data classes to JSON
-#ToDo: 3.) Write json to CMAKEList folder
-#ToDo: 3.) Notify the user that they can acheive the same resulst with a constructed Cmake command (in terms of build systems)
+# Generic type used for parsing in user input while maintaining type hints (Dynamic typing sucks)
+T = TypeVar('T')
+
+
+def get_user_input_for_value(message: str, return_type: Generic[T]) -> T:
+    """Prompts a user for a given value and returns a typed version of the data according to the user provided
+    generic"""
+    #ToDo: Add a system for printing verbose output according to script arguments.
+    return return_type(input(message))
+
+
+def prompt_user_to_add_cmake_var() -> bool:
+    """Prompt a user to determine weather or not to add cmake_vars to the build config and return a boolean."""
+    add_cmake_var: str = get_user_input_for_value("Do you have any Cmake vars to add? (y/n)", str).lower()
+    while add_cmake_var not in ["y", "n"]:
+        print("Invalid choice. Use (y) for yes. (n) for No.")
+        add_cmake_var = get_user_input_for_value("Do you have any Cmake vars to add? (y/n)", str).lower()
+
+    # Return boolean value that represents weather the user wants to add a variable to the configuration
+    return add_cmake_var == "y"
+
+
+def get_cmake_vars_from_user() -> List[CmakeVariable]:
+    """WIP. Get all cmake vars from a given user for a provided build configuration"""
+    cmake_variables: List[CmakeVariable] = []
+    while prompt_user_to_add_cmake_var():
+        cmake_variables.append(CmakeVariable("t", "t", CmakeVarType.BOOL))
+
+    return cmake_variables
+
+
+project_dir: str = get_user_input_for_value("Cmake Project Directory: ", str)
+name: str = get_user_input_for_value("Name: ", str)
+generator: str = get_user_input_for_value("Generator: ", str)
+configurationType: str = get_user_input_for_value("Configuration Type: ", str)
+inheritEnvironments: str = get_user_input_for_value("Inherit Environments: ", str)
+buildRoot: str = r"${projectDir}\\out\\build\\${name}"
+installRoot: str = r"${projectDir}\\out\\install\\${name}"
+cmakeCommandArgs: str = get_user_input_for_value("Cmake Command Args: ", str)
+buildCommandArgs: str = get_user_input_for_value("Build Command Args: ", str)
+ctestCommandArgs: str = get_user_input_for_value("Ctest Command Args: ", str)
+variables: List[CmakeVariable] = []  # ToDo: Parse these in using helper methods
+
+#ToDo: Wrap this in a loop driven by user input
+build_config: BuildConfig = BuildConfig(name=name,
+                                        generator=generator,
+                                        configurationType=configurationType,
+                                        inheritEnvironments=inheritEnvironments,
+                                        buildRoot=buildRoot,
+                                        installRoot=installRoot,
+                                        cmakeCommandArgs=cmakeCommandArgs,
+                                        buildCommandArgs=buildCommandArgs,
+                                        ctestCommandArgs=ctestCommandArgs,
+                                        variables=variables)
+
+
+#ToDo: Parse data classes to JSON and write to the project_dir
+print(json.dumps(asdict(build_config)))
+
+#ToDo: Notify the user that they can acheive the same resulst with a constructed Cmake command (in terms of build systems)
