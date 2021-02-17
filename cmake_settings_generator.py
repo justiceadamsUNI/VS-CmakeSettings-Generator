84ccf6708b8ae2d249c290a2aaf26960a351acd5
Justice Adams <adamsjal@uni.edu>
2021-02-16 19:41:49 -0800

    add loop for buildconfig generation

--- cmake_settings_generator.py
+++ cmake_settings_generator.py
@@ -8,7 +8,7 @@ class NameableEnum(Enum):
     def _generate_next_value_(name, start, count, last_values):
         return name
 
-
+#ToDo: Make enum json serializable
 class CmakeVarType(NameableEnum):
     BOOL = auto()
     FILEPATH = auto()
@@ -34,14 +34,19 @@ class BuildConfig:
     name: str
     generator: str
     configurationType: str
-    inheritEnvironments: str  #ToDo: Convert to an enum after finding microsoft API doc
-    installRoot: str
+    inheritEnvironments: str  # ToDo: Convert to an enum after finding microsoft API doc
     cmakeCommandArgs: str
     buildCommandArgs: str
     ctestCommandArgs: str
     variables: List[CmakeVariable]
     buildRoot: str
     installRoot: str
+
+
+@dataclass(frozen=True)
+class BuildConfigsList:
+    """Data container for representing a list of build configurations. This is the final JSON file."""
+    configurations: List[BuildConfig]
 
 
 # Generic type used for parsing in user input while maintaining type hints (Dynamic typing sucks)
@@ -51,25 +56,25 @@ T = TypeVar('T')
 def get_user_input_for_value(message: str, return_type: Generic[T]) -> T:
     """Prompts a user for a given value and returns a typed version of the data according to the user provided
     generic"""
-    #ToDo: Add a system for printing verbose output according to script arguments.
+    # ToDo: Add a system for printing verbose output according to script arguments.
     return return_type(input(message))
 
 
-def prompt_user_to_add_cmake_var() -> bool:
+def prompt_user_for_yes_no_value(input_prompt: str) -> bool:
     """Prompt a user to determine weather or not to add cmake_vars to the build config and return a boolean."""
-    add_cmake_var: str = get_user_input_for_value("Do you have any Cmake vars to add? (y/n): ", str).lower()
-    while add_cmake_var not in ["y", "n"]:
+    user_input_value: str = get_user_input_for_value(input_prompt, str).lower()
+    while user_input_value not in ["y", "n"]:
         print("Invalid choice. Use (y) for yes. (n) for No.")
-        add_cmake_var = get_user_input_for_value("Do you have any Cmake vars to add? (y/n): ", str).lower()
+        user_input_value = get_user_input_for_value(input_prompt, str).lower()
 
-    # Return boolean value that represents weather the user wants to add a variable to the configuration
-    return add_cmake_var == "y"
+    # Return boolean value that represents yes or no
+    return user_input_value == "y"
 
 
 def get_cmake_vars_from_user() -> List[CmakeVariable]:
     """Get all cmake vars from a given user for a provided build configuration."""
     cmake_variables: List[CmakeVariable] = []
-    while prompt_user_to_add_cmake_var():
+    while prompt_user_for_yes_no_value("Do you have any Cmake vars to add? (y/n): "):
         try:
             cmake_variables.append(CmakeVariable(
                 get_user_input_for_value("Cmake Variable Name: ", str),
@@ -109,13 +114,19 @@ def build_config_from_user_input() -> BuildConfig:
                        variables=variables)
 
 
-
 # Main -------------------------
-v = build_config_from_user_input()
+# ToDo: Clean this code up
+# ToDo: Notify the user that they can acheive the same resulst with a constructed Cmake command (in terms of build systems)
+build_config = build_config_from_user_input()
+build_configs: List[BuildConfig] = [build_config]
 print()
-print(f"Generated Build Config ({v.name}):")
-print(json.dumps(asdict(v), indent=2))
+print(f"Generated Build Config ({build_config.name}):")
+print(json.dumps(asdict(build_config), indent=2))
 
-#ToDo: Parse data classes to JSON and write to the project_dir
+while prompt_user_for_yes_no_value("Do you have any other build configs to setup? (y/n): "):
+    build_config = build_config_from_user_input()
+    print()
+    print(f"Generated Build Config ({build_config.name}):")
+    print(json.dumps(asdict(build_config), indent=2))
 
-#ToDo: Notify the user that they can acheive the same resulst with a constructed Cmake command (in terms of build systems)
+# ToDo: Write to the JSON file to the project_dir
